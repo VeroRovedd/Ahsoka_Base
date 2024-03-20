96f2634122a9ba6d8998cc2d3e06f32c9e92132d
VeroRovedd <vero.edd07@gmail.com>
2024-03-19 23:02:27 -0600

    Testing Ahsoka API (get)

--- index.js
+++ index.js
@@ -1,50 +1,40 @@
-function getData (){
-    fetch("https:jsonplaceholder.typicode.com/todos/1")
-        .then((response) => response.json())
-        .then((json) => console.log(json))
-        .catch((error) => console.log(error));
+function getFormattedDate(date) {
+  let apiDate = new Date(date);
+  let year = apiDate.getFullYear();
+  let month = apiDate.getMonth();
+  let day = apiDate.getDate();
+
+  return `${day}/${month}/${year}`;
 }
 
-const apiURL = "https://jsonplaceholder.typicode.com"
+const apiURL =
+  "https://achievements-app-1-0-0.onrender.com/api/v1/achievements";
+// API Test: https://jsonplaceholder.typicode.com
 const HTML_Response = document.querySelector("#feed");
-const date = new Date();
-const someDay = `${date.getDate()}/${date.getMonth() +1}/${date.getFullYear()}`;
 
-fetch(`${apiURL}/photos`)
+fetch(`${apiURL}/getall`)
   .then((response) => response.json())
-  .then((photos) => {
-    const template = photos.map((photo) =>
-      `<div class="card">
-      <div class="goalHeader">
-        <img class="goalImage" src="${photo.url}"/>
-        <div class="goalInfo">
-          <p class="goalDate">${someDay}</p>
-          <p>${photo.title}</p>
-          <p>${photo.id}</p> 
+  .then((goals) => {
+    console.log(goals);
+    const template = goals.map(
+      (goal) =>
+        `
+        <div class="card">
+          <div class="goalHeader">
+            <img class="goalImage" src="${goal.image}"/>
+            <div class="goalInfo">
+              <p class="goalDate">${getFormattedDate(goal.date)}</p>
+              <p class="goalTitle">${goal.title}</p>
+              <p class="goalArea">${goal.id_tag}</p> 
+            </div>
+          </div>
+          <div class="goalBody">
+            <p>${goal.description}</p>
+          </div>
         </div>
-      </div>
-      <div class="goalBody">
-        <p>Descripción</p>
-      </div>
-    </div>`
+        `
     );
 
     HTML_Response.innerHTML = `${template}`;
-  });
-
-/*
-<div class="card">
-  <div class="goalHeader">
-    <img> </img>
-    <div class="goalInfo">
-      <fecha>
-      <titulo>
-      <area>
-      <likes> 
-    </div>
-  </div>
-  <div class="goalBody">
-    <p>Descripción</p>
-  </div>
-</div>
-*/
+  })
+  .catch((error) => console.log(error.message));
