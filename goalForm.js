bfe4e586e95f64d0c847070cc106dc70fc10466f
VeroRovedd <vero.edd07@gmail.com>
2024-03-20 17:44:22 -0600

    Testing Ahsoka API (post)

--- goalForm.js
+++ goalForm.js
@@ -2,7 +2,7 @@ function getElements (){
     let goalTitle = document.getElementsByName("goalTitle")[0].value;
     let goalArea = document.getElementById("area").value;
     let goalDate = document.getElementById("goalDate").value;
-    let goalDescrption = document.getElementById("goalDescription").value;
+    let goalDescription = document.getElementById("goalDescription").value;
     let goalVisibility = document.getElementById("goalVisibility").value;
     //let goalImage = document.getElementById("goalImage").value;
 
@@ -10,23 +10,36 @@ function getElements (){
     console.log(`Input title: ${goalTitle}`);
     console.log(`Selected area: ${goalArea}`);
     console.log(`Selected Date: ${goalDate}`);
-    console.log(`Goal Description: ${goalDescrption}`);
+    console.log(`Goal Description: ${goalDescription}`);
     console.log(`Goal Visibility: ${goalVisibility}`);
     //console.log(`Goal image: ${goalImage}`).value;
 }
 
+/*function validateData() {
+
+}*/
+
 async function postData() {
+
     try {
+        let goalTitle = document.getElementsByName("goalTitle")[0].value;
+        let goalArea = document.getElementById("area").value;
+        // let goalDate = document.getElementById("goalDate").value;
+        let goalDescription = document.getElementById("goalDescription").value;
+        // let goalVisibility = document.getElementById("goalVisibility").value;
+
         const body = {
-            title: "My title",
-            body: "Hello body",
-            userId: 2
+            title: goalTitle,
+            id_tag: goalArea,
+            description: goalDescription
         };
 
-        const response = await fetch("https:jsonplaceholder.typicode.com/posts",
+        const apiURL = "https://achievements-app-1-0-0.onrender.com/api/v1/achievements"
+        // Test API: "https:jsonplaceholder.typicode.com/posts
+
+        const response = await fetch(`${apiURL}/create`,
             {
                 method: "POST",
-                //body,
                 body: JSON.stringify(body),
                 headers: {
                     "Content-type": "application/json; charset=UTF-8",
